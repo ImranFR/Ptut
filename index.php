092bb5bf254391fe22d172a1fd8ef4e38e037ead
ImranFR <imran.zamoum@gmail.com>
2016-03-14 11:49:41 +0100

    Update for style
    Menu mis en liste pour le css

--- index.php
+++ index.php
@@ -12,11 +12,13 @@
     </head>
     <body>
         <header>
-            <a href="./index.php">Accueil</a>
-            <a href="./ajout.php">Ajout d'un rapport</a>
-            <a href="./tri.php">Recherche de rapport</a>
-            <a href="./admin.php">Page administrateur</a>
-        </header>
-        
+			<ul>
+				<li><a href="./index.php">Accueil</a></li>
+				<li><a href="./ajout.php">Ajout d'un rapport</a></li>
+				<li><a href="./tri.php">Recherche de rapport</a></li>
+				<li><a href="./admin.php">Page administrateur</a></li>
+			</ul>
+		</header>
+       	
     </body>
 </html>
